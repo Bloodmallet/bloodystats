c23e77990f6d03e25e8e7d61b9ebc43d08ae339e
pkaskaround <kruse.peter.1990@gmail.com>
2017-05-05 20:04:31 +0200

    [Fix] python 3

--- Bloodystats.py
+++ Bloodystats.py
@@ -1,4 +1,4 @@
-#!python3
+#!/usr/bin/env python3
 ###############################################################################
 ##
 ## Bloodystats uses SimulationCraft to get the best secondary stat distribution
